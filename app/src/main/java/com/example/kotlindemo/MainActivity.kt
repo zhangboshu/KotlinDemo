905a56d581bf363e1f289568021e0527c8780b90
zhangboshu <zhangboshu2017@163.com>
2018-01-27 14:24:15 +0800

    折叠效果

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,36 +1,78 @@
 package com.example.kotlindemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.button
-import javax.security.auth.login.LoginException
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.BaseAdapter
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_layout)
 
-        val ints = intArrayOf(1, 2, 3)
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//
+//        toolbar.setNavigationOnClickListener {
+//            onBackPressed()
+//        }
 
-        ints.forEach {
-            Log.i("wtf", it.toString())
+        val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        toolbarLayout.title = " "
+
+//        val imageView = findViewById<ImageView>(R.id.ivImage)
+//        imageView.setImageResource(R.mipmap.ic_launcher)
+
+        val viewPager = findViewById<ViewPager>(R.id.viewpager)
+        setupViewPager(viewPager)
+
+        val tabLayout = findViewById<TabLayout>(R.id.sliding_tabs)
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager?) {
+        val adapter = Adapter(supportFragmentManager)
+        adapter.addFragment(Fragment1.newInstance("第一个frag"), "第一个")
+        adapter.addFragment(Fragment1.newInstance("第二个frag"), "第二个")
+        adapter.addFragment(Fragment1.newInstance("第三个frag"), "第三个")
+        viewPager?.adapter = adapter
+
+    }
+
+    private class Adapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+
+        private val fragments = ArrayList<Fragment>()
+        private val fragmentTitles = ArrayList<String>()
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            fragmentTitles.add(title)
         }
 
-        var list = listOf(1, 2, 3)
-        Log.i("wtf", "lastIndex: " + list.lastIndex)
-        Log.i("wtf", "indices" + list.indices)
-
-        for (i in list) {
-            Log.i("wtf", i.toString())
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
         }
 
-        for (i in list.indices){
-            Log.i("wtf", i.toString())
+        override fun getCount(): Int {
+            return fragments.size
         }
 
+        override fun getPageTitle(position: Int): CharSequence? {
+            return fragmentTitles[position]
+        }
     }
 }
 
