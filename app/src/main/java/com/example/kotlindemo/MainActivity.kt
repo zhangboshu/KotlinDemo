6061e7f3bb02d4874b8720ae941aedf3dfc2c0a1
zhangboshu <zhangboshu2017@163.com>
2018-05-28 09:12:54 +0800

    陀螺仪

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,23 +1,60 @@
 package com.example.kotlindemo
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_project.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensor: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_project)
 
-
-        clickMe.setOnClickListener {
-            println(Bean("ZhangBoshu", 20).toString())
-            "zhangboshu".log("lll")
-            log("lalala")
-        }
+//
+//        clickMe.setOnClickListener {
+//            println(Bean("ZhangBoshu", 20).toString())
+//            "zhangboshu".log("lll")
+//            log("lalala")
+//        }
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
 
     }
-}
 
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val values = event.values
+        val builder = StringBuilder()
+        builder.append("x方向旋转")
+        builder.append(values[0])
+        builder.append("\ny方向旋转")
+        builder.append(values[1])
+        builder.append("\nz方向旋转")
+        builder.append(values[2])
+        sensorTv.text = builder.toString()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager.unregisterListener(this)
+    }
+}
 
